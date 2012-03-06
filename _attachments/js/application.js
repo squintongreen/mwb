e7b621c487a6efae070a7bf951482dcc402b2c35
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2012-03-06 19:28:15 +0000

    *** empty log message ***

--- _attachments/js/application.js
+++ _attachments/js/application.js
@@ -3,23 +3,40 @@ $(document).ready(function(){
 
     // FIXME: this stuff is a subject to change
     // as production URLs will probably not contain DB name or design 
-    var path = unescape(document.location.pathname).split('/'),
-    design = path[3], dbname = path[1]
+    var path = unescape(document.location.pathname).split('/')
 
-    $.db = $.couch.db(dbname);
+    $.design = path[3];
+    $.dbname = path[1];
+
+    $.db = $.couch.db($.dbname);
     ////////////////////////////////////////////////
 
     // enerjizing buttons
     // pull-right,pull-left buttons submitting the form
     $(".pull-right,.pull-left") .click(function(e){
-        $("form").submit()
-        e.preventDefault()
+        // submitting the ONLY form on the page
+        var ret = $("form").submit();
+        console.log(ret);
+        e.preventDefault();
+        return false;
     })
+
 
     // processing registration form
     $("#registerForm").submit(function(e){
-        var form = $(this).serializeArray();
-        console.log(form)
-        //$.db.signup()
+        var form = $(this).serializeObject();
+        var user_doc = {
+            name: form.emailAddress,
+            email: form.emailAddress
+        };
+
+        console.log(user_doc)
+        $.couch.signup(user_doc, form.password, {
+            success: function(res){
+                assert(res.ok == true);
+                window.location.replace("wizard.html")
+            }
+        });
+
     })
 });
