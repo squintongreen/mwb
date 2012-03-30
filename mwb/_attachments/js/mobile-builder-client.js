8ab74d375ebd64139f02756e25eed1354fb01f6c
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2012-03-30 10:38:23 +0000

    @svn-commit.tmp

--- mwb/_attachments/js/mobile-builder-client.js
+++
@@ -1,9 +0,0 @@
-mobileBuilderClient = {
-    checkUriAvailability: function(uri){
-        return uri.indexOf("new") == -1
-    },
-    
-    listTemplates: function(){
-        
-    }
-}
--- mwb/views/all-docs/map.js
+++
@@ -1,4 +0,0 @@
-function(doc) {
-    if(doc.type == 'project')
-        emit(doc._id, doc)
-}
