520558cd260027f07e2372773fae2dc1bc6cf194
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2012-04-09 19:22:00 +0000

    MWB-67 r98 merged into Sam's branch

--- mwb/shows/_.js
+++ mwb/shows/_.js
@@ -4,13 +4,20 @@ function(doc, req) {
 
     _ = this._
     // Enables Mustache.js-like templating.    
-    _.templateSettings = {
-        interpolate : /\{\{(.+?)\}\}/g
-    };
+     _.templateSettings = {
+         interpolate : /\{\{(.+?)\}\}/g
+     };
 
     var tmpl = template[req.path[5]]
+
+    processTemplate = function(fileName){
+        var callback = _.template(tmpl[fileName])
+        return callback({req: req, template: tmpl, processTemplate: processTemplate })
+    }
+
+  
     var index = _.template(tmpl['_'])
 
-    send(index({ req: req, template: tmpl, file: req.path[6].replace(/\.html/,'') }))
+    send(index({ template: tmpl, file: req.path[6].replace(/\.html/,''), processTemplate: processTemplate  }))
 
 }
